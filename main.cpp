66ad42a8639f33b0a3de3ef60f01cc8163ed3fb3
Serelllka <44942855+Serelllka@users.noreply.github.com>
2023-03-07 20:44:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ struct TOffset {
 struct TPoint {
     size_t x;
     size_t y;
+    char dest;
 };
 
 struct TConfig {
@@ -25,18 +26,16 @@ TConfig config = {
         10
 };
 
+bool isOver = false;
 std::mutex mtx;
+char destination = 'u';
 
 class TSnake {
 public:
-    TSnake(size_t headX, size_t headY)
-            : _points(std::vector<TPoint>(1))
-    {
-        _points[0].x = headX;
-        _points[0].y = headY;
-    }
+    TSnake()
+    {}
 
-    bool CheckForCollision(size_t x, size_t y) {
+    bool CheckForCollision(size_t x, size_t y) const {
         return std::any_of(_points.begin(), _points.end(), [x, y](const TPoint& a) {
             return a.x == x && a.y == y;
         });
@@ -45,14 +44,30 @@ public:
     void AddToTail(const TPoint& point) {
         _points.push_back(point);
     }
+    
+    TPoint& GetHead() {
+    	return _points[0];
+	}
+	
+	std::vector<TPoint>& GetBody() {
+		return _points;
+	}
+	
+	char GetSymbolByCoord(size_t x, size_t y) const {
+		if (_points[0].x == x && _points[0].y == y) {
+			return '0';
+		}
+		return 'o';
+	} 
+	
 private:
     std::vector<TPoint> _points;
 };
 
 class TField {
 public:
-    TField(size_t _height, size_t _width)
-            : _height(_height), _width(_width)
+    TField(size_t _height, size_t _width, TSnake& snake)
+            : _height(_height), _width(_width), _snake(snake)
     {}
 
     size_t GetHeight() const {
@@ -67,11 +82,15 @@ public:
         if (x == 0 || x == _height - 1 || y == 0 || y == _width - 1) {
             return '#';
         }
+        if (_snake.CheckForCollision(x, y)) {
+        	return _snake.GetSymbolByCoord(x, y);
+		}
         return ' ';
     }
 private:
-    size_t _height;
-    size_t _width;
+    size_t 	_height;
+    size_t 	_width;
+    TSnake& _snake;
 };
 
 class TScore
@@ -116,17 +135,6 @@ public:
         _scores.push_back(score);
     }
 
-//    void GetStringScores()
-//    {
-//        for(int i = 0; i < _scores.size(); i++)
-//        {
-//            std::cout << "Game: " << i << std::endl;
-//            std::cout << _scores[i].GetScoreString() << std::endl;
-//            std::cout << _scores[i].GetTimeString() << std::endl << std::endl;
-//        }
-//        return
-//    }
-
 private:
     std::vector<TScore> _scores;
 };
@@ -151,7 +159,7 @@ private:
 
 class TGameFrame {
 public:
-    TGameFrame(TField field, size_t height, size_t width)
+    TGameFrame(TField& field, size_t height, size_t width)
             :
             _field_offset({0, 0}),
             _main_field(field),
@@ -173,7 +181,7 @@ public:
     }
 private:
     THeadVisualisation _head;
-    TField _main_field;
+    TField& _main_field;
     TOffset _field_offset;
     size_t _height;
     size_t _width;
@@ -184,7 +192,8 @@ private:
         size_t maxFieldH = minFieldH + _main_field.GetHeight();
         size_t minFieldW = _field_offset.y;
         size_t maxFieldW = minFieldW + _main_field.GetWidth();
-        if (x >= minFieldH && x < maxFieldH && y >= minFieldW && y < maxFieldW) {
+        
+		if (x >= minFieldH && x < maxFieldH && y >= minFieldW && y < maxFieldW) {
             return _main_field.GetSymbolByCoord(x - _field_offset.x, y - _field_offset.y);
         }
         return _head.getSymbolByCoord(x, y);
@@ -203,9 +212,7 @@ void UpdateFrame() {
 //        TSnake.isPressed();
 
         FakeConsole();
-
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
     }
 }
 
@@ -240,15 +247,110 @@ void GameOver(){
     mtx.unlock();
 }
 
+void Update(TSnake& snake){
+    bool up     = (GetAsyncKeyState(VK_UP) & 0x80000000);
+    bool down   = (GetAsyncKeyState(VK_DOWN) & 0x80000000);
+    bool right  = (GetAsyncKeyState(VK_RIGHT) & 0x80000000);
+    bool left   = (GetAsyncKeyState(VK_LEFT) & 0x80000000);
+
+	
+    mtx.lock();
+    if (up && destination != 'd') {
+		destination = 'u';
+	} else if (down && destination != 'u') {
+		destination = 'd';
+	} else if (right && destination != 'l') {
+		destination = 'r';
+	} else if (left && destination != 'r') {
+		destination = 'l';
+	}
+
+    
+    decltype(snake.GetBody()) body = snake.GetBody(); 
+	for (size_t i = body.size() - 1; i >= 0; i--) {
+		size_t dx = 0;
+    	size_t dy = 0;
+		 
+		char currDest;
+		if (i == 0) {
+			currDest = destination;
+		} else {
+			currDest = body[i - 1].dest;
+		}
+		switch (currDest) {
+			case 'u':
+				dx = -1;
+			break;
+			case 'd':
+				dx = 1;
+			break;
+			case 'r':
+				dy = 1;
+			break;
+			case 'l':
+				dy = -1;
+			break;	
+		}
+		body[i].dest = currDest;
+		body[i].x += dx;
+		body[i].y += dy;
+		if (i == 0) {
+			break;
+		}
+	}
+
+    mtx.unlock();
+}
+
+void DrawFrameLoop(TGameFrame& frame) {
+	while(true) {
+		frame.DrawFrame();
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		system("cls");
+		mtx.lock();
+		if (isOver) {
+			mtx.unlock();
+			break;
+		}
+		mtx.unlock();
+	}
+	GameOver();
+}
+
+void UpdateLoop(TSnake& snake) {
+	while(true) {
+		Update(snake);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		decltype(snake.GetHead()) head = snake.GetHead();
+		system("cls");
+		mtx.lock();
+		if (head.x == 0 || head.y == 0) {
+			isOver = true;
+			mtx.unlock();
+			break;
+		}
+		mtx.unlock();
+	}
+}
+
 int main() {
-//    TField field(10, 10);
-//    TGameFrame frame(field, 20, 20);
-//    frame.SetFieldOffset({10, 0});
-//    TSnake snake(0, 0);
-//    snake.AddToTail({1, 1});
-//    std::cout << snake.CheckForCollision(1, 0) << std::endl;
-//    frame.DrawFrame();
-    std::thread thr(GameOver);
-    thr.join();
-    return 0;
+	TSnake snake;
+	snake.AddToTail({4, 4, 'l'});
+    snake.AddToTail({4, 5, 'l'});
+    snake.AddToTail({4, 6, 'l'});
+    snake.AddToTail({4, 7, 'l'});
+    snake.AddToTail({4, 8, 'l'});
+    
+	TField field(10, 10, snake);
+    
+	TGameFrame frame(field, 20, 20);
+    
+	frame.SetFieldOffset({10, 0});
+
+	std::thread thr2(DrawFrameLoop, std::reference_wrapper<TGameFrame>(frame));
+	std::thread thr1(UpdateLoop, std::reference_wrapper<TSnake>(snake));
+//    thr.join();
+	thr1.join();
+	thr2.join();
+//    return 0;
 }
